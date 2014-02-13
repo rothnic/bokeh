cae839378b5130a4e2521f80a399017b1ab9811d
hugo <hugo>
2014-02-13 09:43:30 -0500

    remote ocean volume example

---
+++ examples/plotting/server/remote_ocean_volume.py
@@ -0,0 +1,74 @@
+import numpy as np
+from bokeh.plotting import *
+from bokeh.objects import Range1d, MouseSlicer
+
+output_server("remote_volume")
+#output_server("image2")
+source = RemoteDataSource(data_url="/defaultuser/oceantemperature/big.table/big", 
+                          owner_username="defaultuser",
+                          index_slice=[None, None, 0],
+                          data={'x': [0], 
+                                'y': [0], 
+                                'global_offset_x' : [0],
+                                'global_offset_y' : [0],
+                                'global_dw' : [10],
+                                'global_dh' : [10],
+                                'dw' : [10], 
+                                'dh' : [10], 
+                                'palette': ["Spectral-256"]
+                            }
+)
+
+image(
+    source=source,
+    image="image",
+    x="x",
+    y="y",
+    dw="dw",
+    dh="dh",
+    width=600,
+    height=400,
+    palette="palette",
+    x_range=Range1d(start=0, end=10), 
+    y_range=Range1d(start=0, end=10),
+    tools="pan,wheel_zoom,box_zoom,reset,previewsave"
+)
+
+slicer = MouseSlicer(remote_data_source=source,
+                     max_slice=41,
+                     slice_dimension=2)
+session().add(slicer)
+curplot().tools.append(slicer)
+curplot()._dirty = True
+session().store_all()
+
+
+source = RemoteDataSource(data_url="/defaultuser/oceantemperature/big.table/big", 
+                          owner_username="defaultuser",
+                          index_slice=[None, 2000, None],
+                          data={'x': [0], 
+                                'y': [0], 
+                                'global_offset_x' : [0],
+                                'global_offset_y' : [0],
+                                'global_dw' : [10],
+                                'global_dh' : [10],
+                                'dw' : [10], 
+                                'dh' : [10], 
+                                'palette': ["Spectral-256"]
+                            }
+)
+
+image(
+    source=source,
+    image="image",
+    x="x",
+    y="y",
+    dw="dw",
+    dh="dh",
+    width=200,
+    height=600,
+    palette="palette",
+    x_range=Range1d(start=0, end=10), 
+    y_range=Range1d(start=0, end=10),
+    tools="pan,wheel_zoom,box_zoom,reset,previewsave"
+)
